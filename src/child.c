ad53086ad1732a0002f3608717cd84b6e5f5bab8
zz <zengxianzuo@icloud.com>
2016-02-19 16:32:20 +0800

    add some file

---
+++ src/child.c
@@ -0,0 +1,41 @@
+#include "head.h"
+
+void make_child(pchild cptr, int n){
+	int i;
+	int fds[2];
+	int ret;
+	int pid;
+	for(i=0; i<n; i++)
+	{
+		ret = socketpair(AF_LOCAL, SOCK_STREAM, 0, fds);
+		if(-1 == ret)
+		{
+			perror("socketpair");
+			return;
+		}
+		pid = fork();
+		if(pid==0)
+		{
+			close(fds[1]);
+			child_handle(fds[0]);
+		}
+		close(fds[0]);
+		cptr[i].pid = pid;
+		cptr[i].fds = fds[i];
+		cptr[i].busy = 0;
+	}
+}
+
+void child_handle(int fdr)
+{
+	int fd;
+	int flag = 1;
+	while(1)
+	{
+		recv_fd(fdr, &fd);
+		//send_file(fd);
+		write(fdr, &flag, 4);
+	}
+}
+
+send_file(int fd);
---
+++ src/main.c
@@ -0,0 +1,115 @@
+#include "head.h"
+
+int main(int argc, char* argv[])
+{
+	if(argc != 4)
+	{
+		printf("error args\n");
+		return -1;
+	}
+
+	pchild cptr;
+	int num=atoi(argv[3]);
+	cptr = (pchild)calloc(num, sizeof(child));
+	make_child(cptr, num);
+
+	int sfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(-1 == sfd)
+	{
+		perror("socket");
+		return -1;
+	}
+
+	struct sockaddr_in ser;
+	bzero(&ser, sizeof(ser));
+	ser.sin_family = AF_INET;
+	ser.sin_port=htons(atoi(argv[2]));
+	ser.sin_addr.s_addr=inet_addr(argv[1]);
+	int ret;
+	ret = bind(sfd, (struct sockaddr*)&ser, sizeof(struct sockaddr));
+	if(-1 == ret)
+	{
+		perror("bind");
+	}
+
+	listen(sfd, num);
+	int epfd;
+	epfd = epoll_create(1);
+	if(-1 == epfd)
+	{
+		perror("epoll_create");
+		return -1;
+	}
+	struct epoll_event ev, *evs;
+	evs = (struct epoll_event*)calloc(num+1, sizeof(struct epoll_event));
+
+	ev.events = EPOLLIN;
+	ev.data.fd = sfd;
+	ret = epoll_ctl(epfd, EPOLL_CTL_ADD, sfd, &ev);
+	if(-1 == ret)
+	{
+		perror("epoll_ctl");
+		return -1;
+	}
+
+	int i;
+	for(i=0; i<num; i++)
+	{
+		ev.events = EPOLLIN;
+		ev.data.fd = cptr[i].fds;
+		ret = epoll_ctl(epfd, EPOLL_CTL_ADD, cptr[i].fds, &ev);
+		if(-1==ret)
+		{
+			perror("epoll_ctl1");
+			return -1;
+		}
+	}
+
+	int j;
+	int new_fd;
+	int flag;
+	int size;
+	while(1)
+	{
+		ret = epoll_wait(epfd, evs, num+1, -1);
+		if(ret>0)
+		{
+			if(evs[i].data.fd ==sfd && evs[i].events == EPOLLIN)
+			{
+				new_fd = accept(sfd, NULL, NULL);
+				if(-1==new_fd)
+				{
+					perror("accept");
+					return -1;
+				}
+				printf("one client connect, new_fd is %d\n", new_fd);
+				for(j=0; j<num; j++)
+				{
+					if(cptr[j].busy == 0)
+					{
+						break;
+					}
+				}
+				if(j !=num )
+				{
+					send_fd(cptr[j].fds, new_fd);
+					cptr[j].busy = 1;
+					printf("child %d is busy\n", j);
+				}
+
+				for(j=0; j<num; j++)
+				{
+					if(evs[i].data.fd == cptr[j].fds && evs[i].events == EPOLLIN)
+					{
+						size = read(cptr[j].fds, &flag, 4);
+						printf("the size is %d\n", size);
+						cptr[j].busy=0;
+						printf("child %d is not busy\n");
+					}
+				}
+			}
+		}	
+	}
+	return 0;
+}
+
