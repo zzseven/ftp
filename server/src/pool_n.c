0b9dde7b60b67a8ab4bd9ac50e63db50b3290265
zz <zengxianzuo@icloud.com>
2016-02-19 17:23:50 +0800

    itest pushkbkbkbkbkbkbkbkbkbadd puukbkbookbkbolkbol_n.c1kb:wq:wq:kbkb:swkbkbwq:qPkbkrkrkbkbkrkrikbkb.:w:q!

---
+++ server/src/pool_n.c
@@ -0,0 +1,12 @@
+#include "head.h"
+
+void send_n(int sfd, char* p, int len)
+{
+	int total = 0;
+	int size = 0;
+	while(total < len)
+	{
+		size = send(sfd, p+total, len-total, 0);
+		total = total + size;
+	}
+}
